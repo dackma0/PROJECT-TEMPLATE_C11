ac5626ead7131a84a5aca4982c20bbbabc43170d
dackma0 <118316289+dackma0@users.noreply.github.com>
2023-06-15 18:27:22 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,30 @@
-
+var mar, marImg, barco, barcoImg
 function preload(){
+  marImg = loadImage('sea.png')
+  barcoImg = loadAnimation ('ship-1.png', 'ship-2.png', 'ship-3.png', 'ship-4.png')
 
 }
 
 function setup(){
   createCanvas(400,400);
   
+  mar = createSprite (500, 200);
+  mar.addImage(marImg)
+  mar.scale = 0.25
+  mar.velocityX = -3
+  
+  barco = createSprite (200, 200)
+  barco.addAnimation('barquinho', barcoImg)
+  barco.scale = 0.3  
 }
 
 function draw() {
   background("blue");
+  
+  if (mar.x <-100){
+    sea.x = 500
+    
+  }
     drawSprites();
 
  
